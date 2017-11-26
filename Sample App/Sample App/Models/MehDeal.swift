732885942fdf116d7a865b899542c6b314fee876
Christopher Bryan Henderson <bhenderson@startstudio.com>
2017-11-25 17:16:47 -0700

    Renamed some properties on MehDeal to make it obvious that they are markdown.

--- Sample App/Sample App/Models/MehDeal.swift
+++ Sample App/Sample App/Models/MehDeal.swift
@@ -9,13 +9,28 @@
 import Foundation
 
 struct MehDeal: Decodable {
-    let features: String
+    enum CodingKeys: String, CodingKey {
+        case featuresMarkdown = "features"
+        case id
+        case items
+        case photos
+        case purchaseQuantity
+        case title
+        case specificationsMarkdown = "specifications"
+        case story
+        case theme
+        case url
+        case launches
+        case topic
+    }
+    
+    let featuresMarkdown: String
     let id: String
     let items: [MehDealItem]
     let photos: [URL]
     let purchaseQuantity: MehDealPurchaseQuantity
     let title: String
-    let specifications: String
+    let specificationsMarkdown: String
     let story: MehDealStory
     let theme: MehDealTheme
     let url: URL
