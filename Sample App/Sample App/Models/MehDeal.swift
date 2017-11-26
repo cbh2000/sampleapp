42343f3ca159417a5cd1bb52f880c6d760a01705
Christopher Bryan Henderson <bhenderson@startstudio.com>
2017-11-25 17:13:41 -0700

    Added MehDeal implementation. Added MehDealPurchaseQuantity.

--- Sample App/Sample App/Models/MehDeal.swift
+++ Sample App/Sample App/Models/MehDeal.swift
@@ -9,5 +9,16 @@
 import Foundation
 
 struct MehDeal: Decodable {
-    
+    let features: String
+    let id: String
+    let items: [MehDealItem]
+    let photos: [URL]
+    let purchaseQuantity: MehDealPurchaseQuantity
+    let title: String
+    let specifications: String
+    let story: MehDealStory
+    let theme: MehDealTheme
+    let url: URL
+    let launches: [MehDealLaunch]
+    let topic: MehDealTopic
 }
---
+++ Sample App/Sample App/Models/MehDealPurchaseQuantity.swift
@@ -0,0 +1,14 @@
+//
+//  MehDealPurchaseQuantity.swift
+//  Sample App
+//
+//  Created by Christopher Bryan Henderson on 11/25/17.
+//  Copyright © 2017 Christopher Bryan Henderson. All rights reserved.
+//
+
+import Foundation
+
+struct MehDealPurchaseQuantity: Decodable {
+    let maximumLimit: Int
+    let minimumLimit: Int
+}
